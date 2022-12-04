07d962ff9b2d5f506f95e6a46a46c54068970301
dan-2ee <ekdms7273@naver.com>
2022-12-05 04:32:11 +0900

    feat: main page 구현 완료

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,7 +3,17 @@ import * as S from './style';
 
 function Main() {
     return (
-        <>hello</>
+        <S.mainContainer>
+
+                <S.titleContainer>
+                    <S.mainTitle>
+                        POKEMON GO
+                    </S.mainTitle>
+                    <S.subTitle>{`you can \n search for \n Various pokemon`}</S.subTitle>
+                </S.titleContainer>
+                <S.btnStart >get started 🚀</S.btnStart>
+                
+            </S.mainContainer>
     )
 }
 
--- src/pages/Main/style.ts
+++ src/pages/Main/style.ts
@@ -1,4 +1,57 @@
 import styled from 'styled-components';
 
+
 export const mainContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(141.8deg, #ECEEF8 20%, #CCCFDE 50.13%);
+`;
+
+export const titleContainer = styled.div`
+  height: fit-content;
+`;
+
+export const mainTitle = styled.div`
+  font-size: 165px;
+  font-weight: 700;
+  font-family: 'Poppins-Bold';
+  line-height: 250px;
+  position: relative;
+  color: rgba(78, 83, 94, 0.2);
+`;
+
+export const subTitle = styled.div`
+  white-space: pre-line;
+  font-weight: 700;
+  font-size: 60px;
+  font-family: 'Poppins-Bold';
+  line-height: 67px;
+  position: relative;
+  margin-top: -14%;
+  color: #4E535E;
+  mix-blend-mode: normal;
+  opacity: 0.4;
+  text-align: left;
+`;
+
+export const btnStart = styled.button`
+  width: 350px;
+  height: 70px;
+  background: rgba(16, 33, 65, 0.247934);
+  border-radius: 100px;
+  border: none;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 25px;
+  line-height: 26px;
+  text-align: center;
+  color: #FFFFFF;
+  margin-top: 13%;
+  cursor: pointer;
+  box-shadow: 5px 5px rgba(16, 33, 65, 0.3);
 `;
