4ff1419bfd8076512e011043ab07a8ed28e76565
dan-2ee <ekdms7273@naver.com>
2022-12-05 14:27:20 +0900

    feat: api 호출

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { json } from "stream/consumers";
 import * as S from "./style"
 
 function Search() {
+    const [id, setId] = useState<string>("");
+
+    const handleChange = (e: any) => {
+        setId(e.target.value);
+    }
+
+    const getPokemonData = () => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+        })
+    }
     return (
         <S.mainContainer>
             <S.mainContent>
@@ -10,8 +24,8 @@ function Search() {
                 </S.pageTitle>
                 <S.inputContainer>
                     <S.inputTitle >Pokemon Id :</S.inputTitle>
-                    <S.inputBox />
-                    <S.btnSearch >Search</S.btnSearch>
+                    <S.inputBox onChange={handleChange}/>
+                    <S.btnSearch onClick={getPokemonData}>Search</S.btnSearch>
                 </S.inputContainer>
             </S.mainContent>
         </S.mainContainer>
